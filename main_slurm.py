07ae625526229d55d394eaab97304ffe17861faa
Andrew-Hany <andrewhany@aucegypt.edu>
2025-04-10 11:47:58 +0300

    last update

--- main_slurm.py
+++ main_slurm.py
@@ -41,46 +41,46 @@ import time
 
 
 # Define the types for each hyperparameter
-types = [str, float, int, int, int, int, float, float, int]
+# types = [str, float, int, int, int, int, float, float, int]
 
-# Read the Hyperparameter combinations from the file
-with open('combinations.txt', 'r') as f:
-    combinations = [tuple(type_(value) for type_, value in zip(types, line.strip().split(','))) for line in f]
+# # Read the Hyperparameter combinations from the file
+# with open('combinations.txt', 'r') as f:
+#     combinations = [tuple(type_(value) for type_, value in zip(types, line.strip().split(','))) for line in f]
 
-index = int(os.environ['SLURM_ARRAY_TASK_ID'])
-if index >= len(combinations):
-    raise ValueError("SLURM_ARRAY_TASK_ID is out of range")
+# index = int(os.environ['SLURM_ARRAY_TASK_ID'])
+# if index >= len(combinations):
+#     raise ValueError("SLURM_ARRAY_TASK_ID is out of range")
 
-optimizer, learning_rate, batch_size, epsilon, seed, num_epoch, delta, clip_bound, problem = combinations[index]
+# optimizer, learning_rate, batch_size, epsilon, seed, num_epoch, delta, clip_bound, problem = combinations[index]
 
 
-print(f"Running with hyperparameters: optimizer={optimizer}, learning_rate={learning_rate}, batch_size={batch_size}, epsilon={epsilon}, seed={seed}, num_epochs={num_epoch}, delta={delta}, clip_bound={clip_bound}, problem={problem}")
-main_train_wrapper(
-        results_directory='results',
-        delta=delta,
-        learning_rate=learning_rate,
-        clip_bound=clip_bound,
-        batch_size=batch_size,
-        num_epochs=num_epoch,
-        target_epsilon=epsilon,
-        problem_type=problem,
-        optimizer_type=optimizer,
-        seed=seed,
-)
+# print(f"Running with hyperparameters: optimizer={optimizer}, learning_rate={learning_rate}, batch_size={batch_size}, epsilon={epsilon}, seed={seed}, num_epochs={num_epoch}, delta={delta}, clip_bound={clip_bound}, problem={problem}")
+# main_train_wrapper(
+#         results_directory='results',
+#         delta=delta,
+#         learning_rate=learning_rate,
+#         clip_bound=clip_bound,
+#         batch_size=batch_size,
+#         num_epochs=num_epoch,
+#         target_epsilon=epsilon,
+#         problem_type=problem,
+#         optimizer_type=optimizer,
+#         seed=seed,
+# )
 
 
 # # This is on single GPU (not slurm)
-# main_train_wrapper(
-#     results_directory='results',
-#     delta=1e-5,
-#     learning_rate=0.01,
-#     clip_bound=1,
-#     batch_size=512,
-#     num_epochs=2,
-#     target_epsilon=5,
-#     problem_type=2,
-#     optimizer_type='adambc',
-#     seed=1,
-# )
+main_train_wrapper(
+    results_directory='results',
+    delta=1e-5,
+    learning_rate=0.01,
+    clip_bound=1,
+    batch_size=512,
+    num_epochs=1,
+    target_epsilon=5,
+    problem_type=2,
+    optimizer_type='sgd',
+    seed=1,
+)
 
 
